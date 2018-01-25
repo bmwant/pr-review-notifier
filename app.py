a3e3bd50ab7a759d289e7436e53d86a7ac06f50f
Misha Behersky <bmwant@gmail.com>
2018-01-26 01:40:38 +0200

    Add handle on pr accept

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from functools import partial
 from aiohttp import web
 
+import aiohttp
+
+import config
+
 
 async def index(request):
     return web.Response(text='PR review notifier')
@@ -22,9 +26,30 @@ async def handle_pr_event(request):
     return web.Response(text='Ok')
 
 
+async def accept_pr_review(request):
+
+    url = '/repos/{owner}/{repo}/issues/{issue}/labels/{label}'.format(
+        owner=config.OWNER_NAME,
+        repo=config.REPO_NAME,
+        issue=4062,
+        label='bug'
+    )
+
+    api_base = 'https://api.github.com'
+
+    endpoint = '{}{}?access_token={}'.format(api_base, url, config.GITHUB_ACCESS_TOKEN)
+    async with aiohttp.ClientSession() as session:
+        async with session.delete(endpoint) as resp:
+            print(resp.status)
+            print(await resp.text())
+
+    return web.Response(text='Ok')
+
+
 app = web.Application()
 app.router.add_get('/', index)
 app.router.add_post('/payload', handle_pr_event)
+app.router.add_get('/accept', accept_pr_review)
 
 
 if __name__ == '__main__':
