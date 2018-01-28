506ceaf29551b47907bea0a863e2479fae1c8f3f
Misha Behersky <bmwant@gmail.com>
2018-01-28 11:43:48 +0200

    Add config template

---
+++ config_template.py
@@ -0,0 +1,6 @@
+OWNER_NAME = ''  # github organization name
+REPO_NAME = ''  # github repository name
+
+GITHUB_ACCESS_TOKEN = ''  # github token to access api
+
+SLACKBOT_TOKEN = ''  # slack token to send messages
