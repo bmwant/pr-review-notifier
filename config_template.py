ddf71c6ad94830f1394e68974ab0488f9ffb5604
Misha Behersky <bmwant@gmail.com>
2018-01-30 11:42:23 +0200

    Allow config values to be overwritten by env variables

--- config_template.py
+++ config_template.py
@@ -1,6 +1,13 @@
+import os
+
 OWNER_NAME = ''  # github organization name
 REPO_NAME = ''  # github repository name
 
 GITHUB_ACCESS_TOKEN = ''  # github token to access api
 
 SLACKBOT_TOKEN = ''  # slack token to send messages
+
+
+for key, value in globals().copy().items():
+    if key.isupper() and key in os.environ:
+        globals()[key] = os.environ[key]
