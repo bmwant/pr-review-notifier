73e3d4a1d75c2783f632700a272460b10523779b
Misha Behersky <bmwant@gmail.com>
2018-03-12 13:11:15 +0200

    Add test route for debugging

--- database.py
+++ database.py
@@ -54,3 +54,24 @@ async def update_reviews_count(review_id):
             await cur.execute(select_query)
             result = await cur.fetchone()
             return result[0]
+
+
+async def insert_test_record():
+    async with connect() as conn:
+        async with conn.cursor() as cur:
+            date = int(time.time())
+            query_tmpl = (
+                'INSERT INTO reviews(waiting_from, issue_number, pr_name, pr_url) '
+                'VALUES({waiting_from}, {issue_number}, "{pr_name}", "{pr_url}");'
+            )
+            query = query_tmpl.format(
+                waiting_from=date,
+                issue_number=1,
+                pr_name='Test name',
+                pr_url='https://github.com/',
+            )
+            logger.debug('Executing query %s', query)
+            await cur.execute(query)
+            await cur.execute('SELECT last_insert_rowid();')
+            result = await cur.fetchone()
+            return result[0]
